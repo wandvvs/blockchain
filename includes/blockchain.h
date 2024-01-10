26ae5a29598439b3c4faf01a35a28a252fa98d5c
wandvvs <idc@mail.ru>
2024-01-10 23:27:41 +0300

    Now the mining difficulty is defined in the DIFFICULT constant, this will be fixed in the future when I implement the network

--- includes/blockchain.h
+++ includes/blockchain.h
@@ -2,7 +2,8 @@
 #define BLOCKCHAIN_H
 
 #include "block.h"
-#include <vector>
+
+const uint32_t DIFFICULT = 3; // Temporarily it will be const, cuz i dont have network and network config now.
 
 class Blockchain
 {
@@ -10,8 +11,11 @@ private:
     std::vector<Block> chain;
 public:
     Blockchain();
-    void add(Block& block, uint32_t diff);
+
+    void add(Block& block);
     void get_blocks_data();
+
+    bool is_valid();
 };
 
 #endif
--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -2,14 +2,13 @@
 
 Blockchain::Blockchain() = default;
 
-void Blockchain::add(Block& block, uint32_t diff)
+void Blockchain::add(Block& block)
 {
     if(!chain.empty()) {
         std::string prev = chain.back().m_hash;
         block.m_prevhash = prev;
-        block.mine(diff);
     }
-    block.mine(diff);
+    block.mine();
     chain.push_back(block);
 }
 
@@ -18,4 +17,15 @@ void Blockchain::get_blocks_data()
     for(const auto& block : chain) {
         block.get_data();
     }
+}
+
+bool Blockchain::is_valid()
+{
+    for(int i = 0; i < chain.size(); ++i) {
+        if(chain[0].m_hash != chain[1].m_prevhash) {
+            return false;
+        }
+    }
+
+    return true;
 }
