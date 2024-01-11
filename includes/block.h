77b6c17bc9f5bce92a8a4a8da6ed3eadf7e3b324
wandvvs <idc@mail.ru>
2024-01-11 22:16:41 +0300

    added json serialization

--- includes/block.h
+++ includes/block.h
@@ -1,7 +1,11 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+#include <nlohmann/json_fwd.hpp>
+#include <nlohmann/json.hpp>
 #include <openssl/sha.h>
+
+#include "transaction.h"
 
 #include <string>
 #include <cstdint>
@@ -23,18 +27,24 @@ private:
     uint32_t m_index;
     uint32_t m_timestamp;
     int32_t m_nonce;
+    std::vector<Transaction> m_transactions;
         
-    static uint32_t count;
+    static uint32_t count; // It is nedeed for store the number of blocks created and to index the created blocks
 
-    std::string sha256(std::string& combin_data);
+    std::string sha256(std::string& combin_data); // Generating a hash based on the combined data of the block object
     
-    void mine();
-    
+    void mine(); // POW (Proof Of Work) implementation. Generates a hash until it is correct. Increasing the nonce.
 public:
     Block();
-    Block(std::string data);
+    Block(std::string data, std::vector<Transaction> transactions);
 
     void get_data() const;
+    void get_transactions() const;
+
+    nlohmann::json serialize();
+    nlohmann::json serialize_transactions();
+    void deserialize(const nlohmann::json& json);
+    void deserialize_transactions(const nlohmann::json& transactions);
 };
 
 #endif
--- src/block.cpp
+++ src/block.cpp
@@ -1,11 +1,12 @@
 #include "../includes/block.h"
 #include "../includes/blockchain.h"
+#include <nlohmann/json_fwd.hpp>
 
 uint32_t Block::count = 0;
 
 Block::Block() = default;
 
-Block::Block(std::string data) : m_data(data)
+Block::Block(std::string data, std::vector<Transaction> transactions) : m_data(data), m_transactions(transactions)
 {
     this->m_nonce = 0;
     this->m_index = count;
@@ -44,8 +45,7 @@ void Block::mine()
     char_str[DIFFICULT] = '\0';
 
     std::string str(char_str);
-
-    do {
+do {
         combin_data =  std::to_string(this->m_index) + this->m_data +
                               this->m_hash + std::to_string(this->m_nonce) +
                               this->m_prevhash;
@@ -58,5 +58,65 @@ void Block::mine()
 
 void Block::get_data() const
 {
-    std::cout << "Index: " << m_index << "\nNonce: " << m_nonce << "\nHash: " << m_hash << "\nPrevious hash: " << m_prevhash << "\nData: " << m_data << "\nTimestamp: " << m_timestamp << "\n\n" << std::endl;
+    std::cout << "Index: " << m_index << "\nNonce: " << m_nonce << "\nHash: " << m_hash << "\nPrevious hash: " << m_prevhash << "\nData: " << m_data << "\nTimestamp: " << m_timestamp << "\nTransactions count: " << m_transactions.size() << "\n\n";
+}
+
+nlohmann::json Block::serialize()
+{
+    nlohmann::json blockJson;
+
+    blockJson["index"] = m_index;
+    blockJson["data"] = m_data;
+    blockJson["hash"] = m_hash;
+    blockJson["previous hash"] = m_prevhash;
+    blockJson["nonce"] = m_nonce;
+    blockJson["timestamp"] = m_timestamp;
+
+    blockJson["transactions"] = serialize_transactions();
+
+    return blockJson;
+}
+
+nlohmann::json Block::serialize_transactions()
+{
+    nlohmann::json transactions;
+
+    for(const auto& transaction : m_transactions) {
+        transactions.push_back(transaction.serialize());
+    }
+
+    return transactions;
+}
+
+void Block::deserialize(const nlohmann::json& json)
+{
+    m_index = json.value("index", 0);
+    m_data = json.value("data", "");
+    m_hash = json.value("hash", "");
+    m_prevhash = json.value("previous hash", "");
+    m_nonce = json.value("nonce", 0);
+    m_timestamp = json.value("timestamp", 0);
+    
+    deserialize_transactions(json.value("transactions", nlohmann::json::array()));
+}
+
+void Block::deserialize_transactions(const nlohmann::json& transactions)
+{
+    m_transactions.clear();
+
+    for(const auto& transaction : transactions) {
+        Transaction trans;
+        trans.deserialize(transaction);
+        m_transactions.push_back(trans);
+    }
+}
+
+void Block::get_transactions() const
+{
+    for(const auto& transaction : m_transactions) {
+        std::cout << "Sender: "     << transaction.m_sender
+                  << "\nReceiver: " << transaction.m_receiver
+                  << "\nAmount: "   << transaction.m_amount
+                  << "\n\n";
+    }
 }
