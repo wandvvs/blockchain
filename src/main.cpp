ceea418c85038794119c967f387679c07391f1dc
wandvvs <idc@mail.ru>
2024-01-11 22:16:58 +0300

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
 #include "../includes/block.h"
 #include "../includes/blockchain.h"
+#include "../includes/transaction.h"
 
 int main()
 {
-    Blockchain chain;
+    Blockchain chain; /* Creating block chain */
 
-    Block first("hello");
-    Block second("test");
+    Transaction first_trans("Bob", "Alexa", 5416.53); /* Creating some transactions */
+    Transaction second_trans("Tomas", "Robert", 13.2);
 
-    chain.add(first);
-    chain.add(second);
+    std::vector<Transaction> transactions {first_trans, second_trans}; /* Transactions vector for BLOCK constructor */
 
-    chain.get_blocks_data();
-    
-    std::cout << chain.is_valid() << std::endl;
+    Block block("data", transactions); /* Create a block for chain */
+    chain.add(block);
+    block.get_data(); /* Get block data */
+
+    nlohmann::json serialized = block.serialize(); /* Block serialize */
+    std::cout << serialized.dump() << std::endl;
+
+    Block deserialized; /* Block deserialized */
+    deserialized.deserialize(serialized);
+    deserialized.get_data();
 }
