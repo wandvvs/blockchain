cd3c479f54088dbe939d9f98758c33e5df9fff39
wandvvs <idc@mail.ru>
2024-01-28 16:19:02 +0300

    new example

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,33 @@
 #include "../includes/blockchain.h"
 
-int main() {
+// using namespace crypto::rsa;
+
+int main()
+{
     Blockchain chain;
 
-    RSA* sender_private_key = generate_private_key();
-    RSA* sender_public_key = generate_public_key(sender_private_key);
+    RSA* miner_private_key = crypto::rsa::generate_private_key();
 
-    RSA* miner_private_key = generate_private_key();
+    RSA* sender_private_key = crypto::rsa::generate_private_key();
+    RSA* sender_public_key = crypto::rsa::generate_public_key(sender_private_key);
 
-    RSA* receiver_private_key = generate_private_key();
-    RSA* receiver_public_key = generate_public_key(receiver_private_key);
+    RSA* receiver_private_key = crypto::rsa::generate_private_key();
+    RSA* receiver_public_key = crypto::rsa::generate_public_key(receiver_private_key);
 
     Transaction test {"Sender", "Receiver", 250.5};
 
     test.sign(sender_private_key);
     
-    if(test.verify(sender_public_key)) {
+    if(test.verify(sender_public_key))
+    {
         chain.create_transaction(test, sender_private_key);
         chain.mine_pending_transactions("wand", miner_private_key);
 
         std::cout << "Sender balance: " << chain.get_balance("Sender") << std::endl;
         std::cout << "Receiver balance: " << chain.get_balance("Receiver") << std::endl;
     }
-    else {
+    else
+    {
         std::cerr << "Failed to sign transaction" << std::endl;
     }
 
@@ -31,6 +36,8 @@ int main() {
     std::cout << "Miner balance: " << chain.get_balance("wand") << std::endl;
 
     chain.get_blocks_data();
+
+    std::cout << (chain.is_valid() ? "Chain are valid" : "Chain aren`t valid") << std::endl;
 
     return 0;
 }
